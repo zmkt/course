0777cb41dd1b3576c3cad704fc29f1e5f2cd6121
Ilya Kirov <zmkt@mail.ru>
2025-11-03 00:34:07 +0300

    fix code

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -7,12 +7,10 @@ import (
 const (
 	USDToEUR = 0.91
 	USDToRUB = 75.23
+	EURToRUB = USDToRUB / USDToEUR
 )
 
 func main() {
-
-	EURToRUB := USDToRUB / USDToEUR
-
 	fmt.Println("USD to EUR:", USDToEUR)
 	fmt.Println("USD to RUB:", USDToRUB)
 	fmt.Printf("EUR to RUB: %.2f\n", EURToRUB)
