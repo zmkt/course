c40214e36dd4ad65405b24f84fa029633e78abff
Ilya Kirov <zmkt@mail.ru>
2025-11-03 00:27:46 +0300

    домашнее задание - переменные и типы

---
+++ 1-converter/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	USDToEUR = 0.91
+	USDToRUB = 75.23
+)
+
+func main() {
+	fmt.Println("USD to EUR:", USDToEUR)
+	fmt.Println("USD to RUB:", USDToRUB)
+}
