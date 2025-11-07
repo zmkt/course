37b429aa95b8db3077d29c9334e802bcd29504ec
Ilya Kirov <zmkt@mail.ru>
2025-11-08 00:31:44 +0300

    сделал задание

---
+++ 2-calc/main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func renderNumbers() ([]int, error) {
+
+	var numbers []int
+
+	reader := bufio.NewReader(os.Stdin)
+	numbersStr, err := reader.ReadString('\n')
+
+	if err != nil {
+		return nil, err
+	}
+
+	numbersStr = strings.TrimSpace(numbersStr)
+
+	parse := strings.Split(numbersStr, ",")
+
+	for _, value := range parse {
+		numberStr := strings.TrimSpace(value)
+		number, err := strconv.Atoi(numberStr)
+		if err != nil {
+			fmt.Println("Ошибка2")
+			return nil, err
+		}
+		numbers = append(numbers, number)
+	}
+
+	return numbers, nil
+}
+
+func AVG(numbers []int) float64 {
+	var length int = len(numbers)
+	var sum int = 0
+	for _, value := range numbers {
+		sum += value
+	}
+	return float64(sum) / float64(length)
+}
+
+func SUM(numbers []int) int {
+	var sum = 0
+
+	for _, value := range numbers {
+		sum += value
+	}
+
+	return sum
+}
+
+func main() {
+
+	var operation string
+	var numbers []int
+
+	fmt.Print("Введите операцию (AVG/SUM/MED): ")
+
+	fmt.Scan(&operation)
+
+	fmt.Print("Введите числа через запятую: ")
+
+	numbers, err := renderNumbers()
+
+	if err != nil {
+		fmt.Println("Ошибка2")
+		return
+	}
+
+	var result float64
+
+	switch {
+	case operation == "AVG" || operation == "avg":
+		result = AVG(numbers)
+	case operation == "SUM" || operation == "sum":
+		result = float64(SUM(numbers))
+	case operation == "MED" || operation == "med":
+		n := len(numbers)
+		if n%2 == 1 {
+			result = float64(numbers[n/2])
+		} else {
+			result = float64(numbers[n/2-1]+numbers[n/2]) / 2.0
+		}
+	default:
+		fmt.Println("Неправильный оператор")
+	}
+
+	fmt.Println(result)
+
+}
