8081b8d9df3d94449151b5c7fd7bab1c34ff9181
Ilya Kirov <zmkt@mail.ru>
2025-11-15 14:26:47 +0300

    сделал конструктор

--- go-demo-4/main.go
+++ go-demo-4/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand/v2"
 )
 
 type account struct {
@@ -10,18 +11,44 @@ type account struct {
 	url      string
 }
 
+func (acc account) outputPassword() {
+	fmt.Println(acc)
+}
+
+func (acc *account) generatePassword(n int) {
+	res := make([]rune, n)
+	for i := range res {
+		res[i] = letterRunes[rand.IntN(len(letterRunes))]
+	}
+	acc.password = string(res)
+}
+
+func newAccount(login, password, url string) *account {
+
+	
+
+	return &account{
+		url:      url,
+		login:    login,
+		password: password,
+	}
+
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+
 func main() {
 	login := promtData("Введите логин: ")
 	password := promtData("Введите пароль: ")
 	url := promtData("Введите url: ")
 
-	myAccount := account{
-		login:    login,
-		password: password,
-		url:      url,
-	}
 
-	outputPassword(&myAccount)
+
+	myAccount := newAccount(login, password, url)
+
+	myAccount.generatePassword(12)
+
+	myAccount.outputPassword()
 
 }
 
@@ -30,8 +57,4 @@ func promtData(prompt string) string {
 	var res string
 	fmt.Scan(&res)
 	return res
-}
-
-func outputPassword(acc *account) {
-	fmt.Println(acc)
 }
