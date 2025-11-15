f09dd6e8cb2b347dcc079e852b93c6fd32fa3f84
Ilya Kirov <zmkt@mail.ru>
2025-11-15 14:04:11 +0300

    сделал половинру из struct

--- go-demo-4/main.go
+++ go-demo-4/main.go
@@ -4,14 +4,34 @@ import (
 	"fmt"
 )
 
-func main() {
-	a := [4]int{1, 2, 3, 4}
-	reverse(&a)
-	fmt.Println(a)
+type account struct {
+	login    string
+	password string
+	url      string
 }
 
-func reverse(arr *[4]int) {
-	for index, value := range *arr {
-		(*arr)[len(arr)-index-1] = value
+func main() {
+	login := promtData("Введите логин: ")
+	password := promtData("Введите пароль: ")
+	url := promtData("Введите url: ")
+
+	myAccount := account{
+		login:    login,
+		password: password,
+		url:      url,
 	}
+
+	outputPassword(&myAccount)
+
+}
+
+func promtData(prompt string) string {
+	fmt.Print(prompt)
+	var res string
+	fmt.Scan(&res)
+	return res
+}
+
+func outputPassword(acc *account) {
+	fmt.Println(acc)
 }
