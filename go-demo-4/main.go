f363bff7a87afa0700ff928f2800722c6895d75d
Ilya Kirov <zmkt@mail.ru>
2025-11-15 14:52:55 +0300

    сделал раздел struct

--- go-demo-4/main.go
+++ go-demo-4/main.go
@@ -5,12 +5,19 @@ import (
 	"fmt"
 	"math/rand/v2"
 	"net/url"
+	"time"
 )
 
 type account struct {
 	login    string
 	password string
 	url      string
+}
+
+type accountWithTimeStamp struct {
+	account
+	cratedAt  time.Time
+	updatedAt time.Time
 }
 
 func (acc account) outputPassword() {
@@ -25,7 +32,34 @@ func (acc *account) generatePassword(n int) {
 	acc.password = string(res)
 }
 
-func newAccount(login, password, urlString string) (*account, error) {
+// func newAccount(login, password, urlString string) (*account, error) {
+
+// 	if login == "" {
+// 		return nil, errors.New("Неправильный login")
+// 	}
+
+// 	_, err := url.ParseRequestURI(urlString)
+
+// 	if err != nil {
+// 		fmt.Println("Ошибка")
+// 		return nil, errors.New("Неправильный url")
+// 	}
+
+// 	newAcc := &account{
+// 		url:      urlString,
+// 		login:    login,
+// 		password: password,
+// 	}
+
+// 	if password == "" {
+// 		newAcc.generatePassword(123)
+// 	}
+
+// 	return newAcc, nil
+
+// }
+
+func newAccountWithTimeStamp(login, password, urlString string) (*accountWithTimeStamp, error) {
 
 	if login == "" {
 		return nil, errors.New("Неправильный login")
@@ -38,10 +72,14 @@ func newAccount(login, password, urlString string) (*account, error) {
 		return nil, errors.New("Неправильный url")
 	}
 
-	newAcc := &account{
-		url:      urlString,
-		login:    login,
-		password: password,
+	newAcc := &accountWithTimeStamp{
+		cratedAt:  time.Now(),
+		updatedAt: time.Now(),
+		account: account{
+			url:      urlString,
+			login:    login,
+			password: password,
+		},
 	}
 
 	if password == "" {
@@ -59,7 +97,7 @@ func main() {
 	password := promtData("Введите пароль: ")
 	url := promtData("Введите url: ")
 
-	myAccount, err := newAccount(login, password, url)
+	myAccount, err := newAccountWithTimeStamp(login, password, url)
 
 	if err != nil {
 		fmt.Println("Неправильный url или login")
