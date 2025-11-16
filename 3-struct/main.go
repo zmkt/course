8f4a1c9e2481cd72d05aa251bb567549e24e7198
Ilya Kirov <zmkt@mail.ru>
2025-11-16 19:24:07 +0300

    исходник

--- 3-struct/main.go
+++ 3-struct/main.go
@@ -1,12 +1,43 @@
 package main
 
 import (
-	"3-struct/bins"
 	"fmt"
 	"os"
 	"strconv"
 	"time"
 )
+
+type Bin struct {
+	ID       string
+	Private  bool
+	CreateAt time.Time
+	Name     string
+}
+
+func NewBin(id string, private bool, createAt time.Time, name string) Bin {
+	return Bin{
+		ID:       id,
+		Private:  private,
+		CreateAt: createAt,
+		Name:     name,
+	}
+}
+
+type BinList struct {
+	ID        string
+	Private   bool
+	CreatedAt time.Time
+	Name      string
+}
+
+func NewBinList(id string, private bool, createdAt time.Time, name string) BinList {
+	return BinList{
+		ID:        id,
+		Private:   private,
+		CreatedAt: createdAt,
+		Name:      name,
+	}
+}
 
 func main() {
 	args := os.Args
@@ -29,6 +60,6 @@ func main() {
 		return
 	}
 
-	binInput := bins.NewBin(args[1], parsePrivate, createAtParse, args[4])
+	binInput := NewBin(args[1], parsePrivate, createAtParse, args[4])
 	fmt.Println(binInput)
 }
