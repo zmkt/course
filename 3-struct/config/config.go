4d98ff4ba5081c1c9fd9aa30303a319526266ebf
Ilya Kirov <zmkt@mail.ru>
2025-11-28 15:12:30 +0300

    сделал env и конфиг

---
+++ 3-struct/config/config.go
@@ -0,0 +1,17 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+	res := os.Getenv("KEY")
+	fmt.Println(res)
+}
